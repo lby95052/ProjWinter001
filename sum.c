93b19463b567fcfe4e4d3079b49171356cd782fd
lby95052 <914595052@qq.com>
2021-02-13 19:42:40 +0800

    2021/2/13 test code

---
+++ sum.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int sum(int x,int y){
+    return x+y;
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int sum(int x,int y);
+int  main(void){
+    int result;
+    result=sum(2,3);
+    printf( "result =%d\n", result);  
+    getchar();
+}
